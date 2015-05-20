597e6db07d4d269c523594b311bbb35838c3e3e6
renlu <rightlu@outlook.com>
2015-05-20 15:35:18 +0800

    add first

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -8,7 +8,7 @@ function printInventory(inputs) {
         '，单价：' + inputs[i].price.toFixed(2) + '(元)'+
         '，小计：' + (parseInt(inputs[i].price)*parseInt(inputs[i].count)).toFixed(2) +'(元)\n';
 
-        sum += (parseInt(inputs[i].price)*parseInt(inputs[i].count));
+        sum += (parseInt(inputs[i].price)*parseInt(inputs[i].count)).toFixed(2);
     }
     result += '----------------------\n';
     result += '总计：'+sum.toFixed(2)+'(元)\n';
