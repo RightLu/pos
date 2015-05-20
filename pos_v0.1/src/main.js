91ffaeaf254fb3932bb8e091770f39f9a01cc948
renlu <rightlu@outlook.com>
2015-05-20 17:09:59 +0800

    add second

--- pos_v0.1/src/main.js
+++ pos_v0.1/src/main.js
@@ -1,4 +1,37 @@
-//TODO: Please write code in this file.
 function printInventory(inputs) {
+    var result = '***<没钱赚商店>购物清单***\n';
+    var sum_list = [];
 
+    for(var i = 0; i < inputs.length; i++) {
+        var exist = false;
+        var temp = {};
+
+        for(var j = 0; j < sum_list.length; j++) {
+            if (inputs[i].name === sum_list[j].name) {
+                sum_list[j].count++;
+                exist = true;
+            }
+        }
+        if(!exist) {
+            temp.name = inputs[i].name;
+            temp.unit = inputs[i].unit;
+            temp.price = inputs[i].price;
+            temp.count = 1;
+            sum_list.push(temp);
+        }
+    }
+    var sum=0;
+
+    for(var x = 0; x < sum_list.length; x++) {
+        result += '名称：'+sum_list[x].name +
+        '，数量：' + sum_list[x].count+sum_list[x].unit +
+        '，单价：' + sum_list[x].price.toFixed(2)+'(元)' +
+        '，小计：' + (parseInt(sum_list[x].price)*parseInt(sum_list[x].count)).toFixed(2)+'(元)\n';
+
+        sum = sum+sum_list[x].price*sum_list[x].count;
+    }
+    result += '----------------------\n'
+    result += '总计：'+sum.toFixed(2)+'(元)\n';
+    result += '**********************';
+    console.log(result);
 }
