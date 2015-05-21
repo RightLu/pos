72b4014385fb1b78f457369517e2cc722fd36b9e
renlu <rightlu@outlook.com>
2015-05-21 13:07:57 +0800

    add second

--- pos_v0.1/src/main.js
+++ pos_v0.1/src/main.js
@@ -2,17 +2,18 @@ function printInventory(inputs) {
     var result = '***<没钱赚商店>购物清单***\n';
     var sum_list = [];
 
-    for(var i = 0; i < inputs.length; i++) {
+    for (var i=0; i<inputs.length; i++) {
         var exist = false;
         var temp = {};
 
-        for(var j = 0; j < sum_list.length; j++) {
+        for (var j=0; j<sum_list.length; j++) {
             if (inputs[i].name === sum_list[j].name) {
                 sum_list[j].count++;
                 exist = true;
             }
         }
-        if(!exist) {
+
+        if (!exist) {
             temp.name = inputs[i].name;
             temp.unit = inputs[i].unit;
             temp.price = inputs[i].price;
@@ -20,18 +21,19 @@ function printInventory(inputs) {
             sum_list.push(temp);
         }
     }
-    var sum=0;
 
-    for(var x = 0; x < sum_list.length; x++) {
-        result += '名称：'+sum_list[x].name +
-        '，数量：' + sum_list[x].count+sum_list[x].unit +
-        '，单价：' + sum_list[x].price.toFixed(2)+'(元)' +
-        '，小计：' + (parseInt(sum_list[x].price)*parseInt(sum_list[x].count)).toFixed(2)+'(元)\n';
+    var total_price = 0;
+    for (var x=0; x<sum_list.length; x++) {
+        result += '名称：'+ sum_list[x].name + '，' +
+        '数量：' + sum_list[x].count+sum_list[x].unit + '，' +
+        '单价：' + sum_list[x].price.toFixed(2) + '(元)' + '，' +
+        '小计：' + (sum_list[x].price * sum_list[x].count).toFixed(2)+'(元)\n';
 
-        sum = sum+sum_list[x].price*sum_list[x].count;
+        total_price += sum_list[x].price * sum_list[x].count;
     }
-    result += '----------------------\n'
-    result += '总计：'+sum.toFixed(2)+'(元)\n';
+
+    result += '----------------------\n';
+    result += '总计：' + total_price.toFixed(2) + '(元)\n';
     result += '**********************';
     console.log(result);
 }
